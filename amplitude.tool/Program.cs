846714a670f23e4907d200fb35d10235e9f7135e
dcardozo <diiego.cardozo@gmail.com>
2021-09-17 23:31:45 -0300

    Add mechanism to prevent app begin closed

--- amplitude.tool/Program.cs
+++ amplitude.tool/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Reactive;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Threading;
 using amplitude.tool.Users.UnityDelivery.Views;
 
 namespace amplitude.tool
@@ -8,13 +10,27 @@ namespace amplitude.tool
     class Program
     {
         static UserView userView;
+        static bool stuck = true;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             userView = new UserView();
-            
             userView.Init();
+
+            CloseOn();
+            ExitWhenNotStuck();   
+        }
+
+        static void CloseOn() =>
+            Observable.Return(Unit.Default)
+                .Delay(TimeSpan.FromSeconds(10))
+                .SubscribeOn(Scheduler.CurrentThread)
+                .Subscribe(_ => stuck = false);
+
+        static void ExitWhenNotStuck()
+        {
+            while (stuck) 
+                Thread.Sleep(TimeSpan.FromSeconds(1));
         }
     }
 }
