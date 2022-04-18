2e74e8c7f1b6eebc59cb3c7532786a009128d565
dcardozo <diiego.cardozo@gmail.com>
2022-04-17 21:44:59 -0300

    WIP to add event properties to expected events

--- amplitude.tool/Users/Infrastructure/Dtos/EventDto.cs
+++ amplitude.tool/Users/Infrastructure/Dtos/EventDto.cs
@@ -8,7 +8,6 @@ namespace amplitude.tool.Users.Infrastructure.Dtos
     {
         public long event_id { get; set; }
         public string event_type { get; set; }
-        // public Dictionary<string, string> event_properties { get; set; }
-        // public Dictionary<string, string> user_properties { get; set; }
+        public Dictionary<string, object> event_properties { get; set; }
     }
 }
